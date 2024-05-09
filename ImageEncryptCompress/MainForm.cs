f38632fdc4284705451887870f2737956ad57567
Mohanad <mohanadkhaled87@gmail.com>
2024-05-09 21:55:38 +0300

    added saving image after encryption,decryption

--- ImageEncryptCompress/MainForm.cs
+++ ImageEncryptCompress/MainForm.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,6 +32,8 @@ namespace ImageEncryptCompress
         private void OpenImageButton_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "bmp files (*.bmp)|*.bmp|All files (*.*)|*.*";
+            openFileDialog1.RestoreDirectory = true;
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 //Open the browsed image and display it
@@ -54,15 +57,8 @@ namespace ImageEncryptCompress
                 MessageBox.Show("Please open an image first", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
-            //take two input from user to help in encryption process
             string userInput_initial_seed = initial_seed.Text;
             string userInput_tap_position = tap_position.Text;
-
-            /*if(userInput_initial_seed.Length < 8)
-            {
-                MessageBox.Show("Initial seed must be at least 8 bits", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }*/
 
 
             if (userInput_initial_seed == "" || userInput_tap_position == "" || userInput_initial_seed == null || userInput_tap_position == null)
@@ -85,6 +81,7 @@ namespace ImageEncryptCompress
             forwardTime += stopwatch.ElapsedMilliseconds;
             ImageOperations.DisplayImage(EncryptedImage, pictureBox2);
             MessageBox.Show("ENCRYPTION DONE after " + forwardTime + " ms");
+            SaveImage(pictureBox2);
 
         }
 
@@ -97,29 +94,7 @@ namespace ImageEncryptCompress
                 MessageBox.Show("Please encrypt the image first", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
-            /*
-            string userInput_initial_seed = initial_seed.Text;
-            string userInput_tap_position = tap_position.Text;
-
-            if (userInput_initial_seed.Length < 8)
-            {
-                MessageBox.Show("Initial seed must be at least 8 bits", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
-
-            if (userInput_initial_seed == "" || userInput_tap_position == "" || userInput_initial_seed == null || userInput_tap_position == null)
-            {
-                MessageBox.Show("Please enter the initial seed and tap position", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
-
-            int parsedTapPosition = int.Parse(userInput_tap_position);
-            if(parsedTapPosition >= userInput_initial_seed.Length)
-            {
-                MessageBox.Show("Invalid Tap Position", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
-*/
+            
             stopwatch.Start();
             DecryptedImage = ImageEncryption.Decrypt(EncryptedImage, initialSeed, tapPosition);
             stopwatch.Stop();
@@ -128,7 +103,7 @@ namespace ImageEncryptCompress
             MessageBox.Show("DECRYPTION DONE after " + stopwatch.ElapsedMilliseconds + " ms");
             MessageBox.Show("BACKWARD TIME TOTAL : " + backwardTime);
             backwardTime = 0;
-
+            SaveImage(pictureBox2);
         }
         private void compressbtn_Click(object sender, EventArgs e)
         {
@@ -159,7 +134,7 @@ namespace ImageEncryptCompress
                     MessageBox.Show("Unexpected Error!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     return;
                 }
-                sizeBeforCompressionBytes = ((double)ImageOperations.GetHeight(EncryptedImage) * (double)ImageOperations.GetWidth(EncryptedImage) * 24.0) / (8.0);
+                sizeBeforCompressionBytes = ((double)ImageOperations.GetHeight(EncryptedImage) * (double)ImageOperations.GetWidth(EncryptedImage) * 3.0);
                 stopwatch.Start();
                 sizeAfterCompressionBytes = 1024.0 * ImageCompression.Compress(EncryptedImage, CompressedFilePath, initialSeed, tapPosition);
                 stopwatch.Stop();
@@ -169,64 +144,7 @@ namespace ImageEncryptCompress
                 MessageBox.Show("ELAPSED TIME AFTER FORWARD TOTAL IS : " + forwardTime);
                 forwardTime = 0;
             }
-            /*else if(DeviceImageCompress.Checked)
-            {
-                string userInput_initial_seed = initial_seed.Text;
-                string userInput_tap_position = tap_position.Text;
-
-                if (userInput_initial_seed.Length < 8)
-                {
-                    MessageBox.Show("Initial seed must be at least 8 bits", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return;
-                }
-
-
-                if (userInput_initial_seed == "" || userInput_tap_position == "" || userInput_initial_seed == null || userInput_tap_position == null)
-                {
-                    MessageBox.Show("Please enter the initial seed and tap position", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return;
-                }
-                int parsedTapPosition = int.Parse(userInput_tap_position);
-                if (parsedTapPosition >= userInput_initial_seed.Length)
-                {
-                    MessageBox.Show("Invalid Tap Position", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return;
-                }
-
-
-                FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
-                string CompressedFilePath;
-                if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
-                {
-                    CompressedFilePath = folderBrowserDialog1.SelectedPath + "\\CompressedImage.bin";
-                    if (CompressedFilePath == null || CompressedFilePath.Equals(""))
-                    {
-                        MessageBox.Show("Please select a folder to create the compressed file at!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        return;
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("Unexpected Error!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return;
-                }
-                OpenFileDialog openFileDialog1 = new OpenFileDialog();
-                if (openFileDialog1.ShowDialog() == DialogResult.OK)
-                {
-                    //Open the browsed image and display it
-                    string OpenedFilePath = openFileDialog1.FileName;
-                    if (OpenedFilePath == null || OpenedFilePath.Equals(""))
-                    {
-                        MessageBox.Show("Please select an image", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        return;
-                    }
-                    OriginalImage = ImageOperations.OpenImage(OpenedFilePath);
-                    openFileDialog1.Dispose();
-                    sizeAfterCompressionKB = ImageCompression.Compress(OriginalImage, CompressedFilePath,userInput_initial_seed,parsedTapPosition);
-                    sizeBeforCompressionKB = ((double)ImageOperations.GetHeight(OriginalImage) * (double)ImageOperations.GetWidth(OriginalImage) * 24.0) / (8.0 * 1024.0);
-                    ImageOperations.DisplayImage(OriginalImage, pictureBox2);
-                }
-            }*/
+            
         }
 
         private void Decompressbtn_Click(object sender, EventArgs e)
@@ -236,8 +154,6 @@ namespace ImageEncryptCompress
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-
-                //Open the browsed image and display it
                 string OpenedFilePath = openFileDialog1.FileName;
                 stopwatch.Start();
                 EncryptedImage = ImageCompression.Decompress(OpenedFilePath, out string outInitialSeed, out int outTapPosition);
@@ -263,5 +179,18 @@ namespace ImageEncryptCompress
                 MessageBox.Show("The images do not match!", "Image Comparison Result", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
+
+        private void SaveImage(PictureBox pictureBox)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "bmp files (*.bmp)|*.bmp|All files (*.*)|*.*";
+            saveFileDialog1.RestoreDirectory = true;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox.Image.Save(saveFileDialog1.FileName, ImageFormat.Bmp);
+            }
+
+        }
+
     }
 }
